075bbaa18a3efdbbca33c72672186777bade7044
Mateo Covacho <mateocovacho@gmail.com>
2025-03-11 18:41:01 +0000

    sadasd

--- Mateo_App_examen_DINT/Mateo_App_examen_DINTApp.swift
+++ Mateo_App_examen_DINT/Mateo_App_examen_DINTApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct Mateo_App_examen_DINTApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            	ContentView()
         }
     }
 }
